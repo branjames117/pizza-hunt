90f97718d6a2fd63a03f18501a1900d432678f6d
Brandon Hoskins <branjames117@gmail.com>
2022-02-06 18:14:14 -0600

    finished reply schema, controller, and routes

--- routes/api/comment-routes.js
+++ routes/api/comment-routes.js
@@ -1,6 +1,8 @@
 const router = require('express').Router();
 const {
   addComment,
+  addReply,
+  removeReply,
   removeComment,
 } = require('../../controllers/comment-controller');
 
@@ -8,6 +10,9 @@ const {
 router.route('/:pizzaId').post(addComment);
 
 // /api/comments/:pizzaId/:commentId
-router.route('/:pizzaId/:commentId').delete(removeComment);
+router.route('/:pizzaId/:commentId').put(addReply).delete(removeComment);
+
+// delete comment reply
+router.route('/:pizzaId/:commentId/:replyId').delete(removeReply);
 
 module.exports = router;
